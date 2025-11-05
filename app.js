9eb060d9abe8248d4cd123020b4a612da7f2f0bf
Anton <chukanil@gmail.com>
2025-11-05 18:53:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
   const statusEl = document.getElementById('status');
   const randomBtn = document.getElementById('randomBtn');
   const reloadBtn = document.getElementById('reloadBtn');
-  const hfTokenInput = document.getElementById('hfToken');
+  const hfTokenInput = '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';
   const reviewArea = document.getElementById('reviewArea');
   const reviewTextEl = document.getElementById('reviewText');
   const sentIcon = document.getElementById('sentIcon');
